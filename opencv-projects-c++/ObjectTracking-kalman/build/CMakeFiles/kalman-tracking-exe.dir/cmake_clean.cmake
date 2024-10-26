0fd3d175928e2dcf7a0a082dcde71d850fcd6129
Ömer Faruk Günaydın <siromermer@gmail.com>
2024-10-27 00:58:41 +0300

    kalman c++ implementation

---
+++ opencv-projects-c++/ObjectTracking-kalman/build/CMakeFiles/kalman-tracking-exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kalman-tracking-exe.dir/kalman+fast-tracking-predicting.cpp.o"
+  "CMakeFiles/kalman-tracking-exe.dir/kalman+fast-tracking-predicting.cpp.o.d"
+  "kalman-tracking-exe"
+  "kalman-tracking-exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kalman-tracking-exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
